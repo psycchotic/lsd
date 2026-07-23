4822cb1004fa97cec9e025980e321c9776272bd6
psycho <victoryrobot121@mail.ru>
2026-07-23 20:54:29 +0700

    Simplify ISR stub macros

--- kernel/src/io/isr_stubs.asm
+++ kernel/src/io/isr_stubs.asm
@@ -1,28 +1,25 @@
-; --- idt.asm ---
 bits 64
 
-; Macro for exceptions that DO NOT push an error code automatically
 %macro EXCEPTION_NO_ERR_CODE 1
 global exception_handler_%1
 exception_handler_%1:
-    push qword 0    ; Dummy error code to keep stack uniform
-    push qword %1   ; Push the interrupt number
+    push qword 0       push qword %1 
     jmp common_exception_wrapper
 %endmacro
 
-; Macro for exceptions that DO push an error code automatically
+
 %macro EXCEPTION_WITH_ERR_CODE 1
 global exception_handler_%1
 exception_handler_%1:
-                    ; Error code is already pushed by the CPU
-    push qword %1   ; Push the interrupt number
+                    
+    push qword %1  
     jmp common_exception_wrapper
 %endmacro
 
 ; Generate handlers 0 to 31
 %assign i 0
 %rep 32
-    ; Check if 'i' is an exception that pushes an error code
+
     %if i == 8 || (i >= 10 && i <= 14) || i == 17 || i == 21 || i == 29 || i == 30
         EXCEPTION_WITH_ERR_CODE i
     %else
@@ -34,7 +31,7 @@ exception_handler_%1:
 extern c_exception_handler
 
 common_exception_wrapper:
-    ; Save all general-purpose registers (System V AMD64 ABI)
+
     push rbp
     push r15
     push r14
@@ -51,13 +48,12 @@ common_exception_wrapper:
     push rbx
     push rax
 
-    ; Pass the stack pointer as the first argument to the C function (rdi)
     mov rdi, rsp
     
-    ; Call the C handler
+
     call c_exception_handler
 
-    ; Restore all general-purpose registers
+
     pop rax
     pop rbx
     pop rcx
@@ -75,7 +71,7 @@ common_exception_wrapper:
     pop rbp
 
 
-    add rsp, 16     
+    add rsp, 16
     
     iretq
 
